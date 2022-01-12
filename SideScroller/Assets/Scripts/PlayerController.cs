5fde6d8973baed6cff108f6d2a53508cf9625041
Subash <it.subashaa@gmail.com>
2022-01-12 09:33:32 +0530

    RagDoll test

--- SideScroller/Assets/Scripts/PlayerController.cs
+++ SideScroller/Assets/Scripts/PlayerController.cs
@@ -11,15 +11,27 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        animator.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool ragDollActivate = Input.GetKey("space");
+
         if(velocityZ <= 1)
         {
             velocityZ += Time.deltaTime * acceleration;
         }
+        if (ragDollActivate)
+        {
+            animator.enabled = false;
+        }
+        else if (!ragDollActivate)
+        {
+            animator.enabled = true;
+        }
+
         animator.SetFloat("VelocityZ", velocityZ);
     }
 }
